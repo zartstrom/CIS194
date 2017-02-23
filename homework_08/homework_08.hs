2d476364d2a963de047c715b986a58ecd2aa59ef
zartstrom <phj.hitzler@gmail.com>
2017-02-23 23:36:26 +0100

    treeFold

--- homework_08/homework_08.hs
+++ homework_08/homework_08.hs
@@ -18,6 +18,24 @@ moreFun :: GuestList -> GuestList -> GuestList
 moreFun (GL es fun1) (GL fs fun2) = if (fun1 > fun2) then GL es fun1 else GL fs fun2
 
 
-treeFold :: (a -> [b] -> b) -> Tree a -> b
-treeFold = go where
-    go (Node x ts) = f x (map go ts)
+treeFold :: b -> (a -> [b] -> b) -> Tree a -> b
+treeFold e _ (Node a []) = e
+treeFold e f (Node a fs) = f a (map (treeFold e f) fs)
+{-treeFold = go where-}
+{-    go (Node x ts) = f x (map go ts)-}
+
+
+combineGLs :: Employee -> [GuestList] -> GuestList
+combineGLs e gs = moreFun (foldr mappend mempty gs) (foldr mappend (GL [e] (empFun e)) gs) 
+
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e glt = 
+    let withBoss = foldr mappend (GL [e] (empFun e)) (map snd glt)
+        withoutBoss = foldr mappend mempty (map fst glt)
+    in (withBoss, withoutBoss)
+
+maxFun :: Tree Employee -> GuestList
+maxFun tr = 
+    let pair = treeFold (mempty, mempty) nextLevel tr
+    in moreFun (fst pair) (snd pair)
